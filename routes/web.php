924f7e67b4da89576278077d042ccee555b5eb57
Sebastián Rubio <sebastian.rubio12@hotmail.com>
2021-02-05 15:05:30 -0500

    Quite la ruta /index

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,6 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/index', function () {
-    return view('index');
-});
-
 Route::get('/about', function () {
     return view('about');
 });
